48d04910ffbe1e7e71e7a86afe87a12a59d8cec2
zorcal <zorcal.dev@gmail.com>
2025-04-05 19:47:52 +0200

    nvim: add neovim config

---
+++ nvim/.config/nvim/lua/zorcal/plugins/lsp.lua
@@ -0,0 +1,101 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+      require("vim.lsp.log").set_format_func(vim.inspect)
+
+      local lspconfig = require "lspconfig"
+
+      local servers = {
+        bashls = true,
+        gopls = true,
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = {
+                  [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                  [vim.fn.stdpath "config" .. "/lua"] = true,
+                },
+              },
+            },
+          },
+        },
+        rust_analyzer = true,
+        cssls = true,
+        ts_ls = true,
+        emmet_ls = {
+          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+        },
+        pyright = true,
+        zls = true,
+        sqlls = true,
+        terraformls = true,
+      }
+
+      require("mason").setup {}
+
+      require("mason-tool-installer").setup {}
+
+      for name, config in pairs(servers) do
+        if not config then
+          return
+        end
+        if type(config) ~= "table" then
+          config = {}
+        end
+        lspconfig[name].setup(config)
+      end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+          -- Turn off LSP highlighting...
+          client.server_capabilities.semanticTokensProvider = nil
+
+          -- Use built-in LSP omnifunc for completion
+          vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          local opts = { buffer = bufnr, remap = false }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>li", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "]d", function()
+            vim.diagnostic.jump { count = 1, float = true }
+          end, opts)
+          vim.keymap.set("n", "[d", function()
+            vim.diagnostic.jump { count = -1, float = true }
+          end, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+          -- Some lsp keymaps are configured in fzf config
+        end,
+      })
+
+      vim.diagnostic.config {
+        virtual_text = {
+          source = true, -- Show source in virtual text
+        },
+        float = {
+          source = true, -- Show source in floating window
+        },
+      }
+    end,
+  },
+}
---
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -0,0 +1,97 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local zorcal_group = augroup("zorcal", {})
+
+autocmd("TextYankPost", {
+  group = zorcal_group,
+  pattern = "*",
+  desc = "Highlight yank",
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch",
+      timeout = 40,
+    }
+  end,
+})
+
+autocmd("VimResized", {
+  group = zorcal_group,
+  pattern = "*",
+  desc = "Auto equalize splits when Vim is resized",
+  command = "wincmd =",
+})
+
+autocmd({ "BufWritePre" }, {
+  group = zorcal_group,
+  pattern = "*",
+  desc = "Remove extra whitespace",
+  command = [[%s/\s\+$//e]],
+})
+
+autocmd({ "BufRead", "BufNewFile" }, {
+  group = zorcal_group,
+  pattern = "*",
+  desc = "Those damn format options...",
+  command = [[setlocal formatoptions-=cro]],
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function(args)
+    vim.treesitter.stop(args.buf)
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   callback = function()
+--     if vim.o.background == "light" then
+--       vim.api.nvim_set_hl(0, "Normal", {
+--         fg = "#202224",
+--         bg = "#ffffdd",
+--       })
+--     else
+--       vim.api.nvim_set_hl(0, "Normal", {
+--         fg = "#f0f1f2",
+--         bg = "#253443",
+--       })
+--     end
+--
+--     local link_to_normal = {
+--       "String",
+--       "Function",
+--       "Identifier",
+--       "Type",
+--       "Keyword",
+--       "Statement",
+--       "Conditional",
+--       "Repeat",
+--       "Operator",
+--       "Number",
+--       "Boolean",
+--       "Float",
+--       "Constant",
+--       "Character",
+--       "Label",
+--       "PreProc",
+--       "Include",
+--       "Define",
+--       "Macro",
+--       "PreCondit",
+--       "StorageClass",
+--       "Structure",
+--       "Typedef",
+--       "Special",
+--       "SpecialChar",
+--       "Delimiter",
+--       "SpecialComment",
+--       "Underlined",
+--       "Ignore",
+--       "Error",
+--       "Todo",
+--       "Comment",
+--     }
+--     for _, group in ipairs(link_to_normal) do
+--       vim.api.nvim_set_hl(0, group, { link = "Normal" })
+--     end
+--   end,
+-- })
