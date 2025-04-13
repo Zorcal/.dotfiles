c5f3e321545dd4a626ca65c6d5ee215d27fac9f3
zorcal <zorcal.dev@gmail.com>
2025-04-13 10:24:01 +0200

    nvim: update lazy setup and update plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,30 +1,25 @@
---[[
--- Setup initial configuration,
---
--- Primarily just download and execute lazy.nvim
---]]
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  }
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
--- Add lazy to the `runtimepath`, this allows us to `require` it.
----@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- Set up lazy, and load my `lua/zorcal/plugins/` folder
-require("lazy").setup({ import = "zorcal/plugins" }, {
-  change_detection = {
-    notify = false,
+require("lazy").setup({
+  spec = {
+    { import = "zorcal/plugins" },
   },
 })
