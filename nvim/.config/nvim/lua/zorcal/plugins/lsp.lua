4c03a834a2869fd54a549ed10239b0d868eca22a
zorcal <zorcal.dev@gmail.com>
2025-04-13 10:11:42 +0200

    nvim: use 0.11 default keybinds for lsp

--- nvim/.config/nvim/lua/zorcal/plugins/lsp.lua
+++ nvim/.config/nvim/lua/zorcal/plugins/lsp.lua
@@ -69,21 +69,9 @@ return {
 
           local opts = { buffer = bufnr, remap = false }
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
           vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
           vim.keymap.set("n", "<leader>li", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "]d", function()
-            vim.diagnostic.jump { count = 1, float = true }
-          end, opts)
-          vim.keymap.set("n", "[d", function()
-            vim.diagnostic.jump { count = -1, float = true }
-          end, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
           -- Some lsp keymaps are configured in fzf config
         end,
       })
