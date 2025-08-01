a22e0a244db9acfe5998d278efc91d8591995bf3
zorcal <zorcal.dev@gmail.com>
2025-08-01 19:07:51 +0200

    nvim: smart comments using treesitter

--- nvim/.config/nvim/lua/zorcal/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/zorcal/plugins/treesitter.lua
@@ -43,4 +43,23 @@ return {
       }
     end,
   },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("ts_context_commentstring").setup {
+        enable = true,
+        enable_autocmd = false,
+      }
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "JoosepAlviste/nvim-ts-context-commentstring" },
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
 }
