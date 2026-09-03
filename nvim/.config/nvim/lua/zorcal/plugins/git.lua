61f025014dc6397afcbeb18b3b72ec3487334ee3
zorcal <zorcal.dev@gmail.com>
2026-09-03 21:26:22 +0200

    nvim: toggle git diff view

--- nvim/.config/nvim/lua/zorcal/plugins/git.lua
+++ nvim/.config/nvim/lua/zorcal/plugins/git.lua
@@ -15,6 +15,28 @@ return {
     end,
   },
   {
+    "sindrets/diffview.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("diffview").setup {
+        keymaps = {
+          disable_defaults = true,
+        },
+      }
+
+      vim.keymap.set("n", "<leader>gd", function()
+        local lib = require "diffview.lib"
+        local view = lib.get_current_view()
+
+        if view then
+          vim.cmd "DiffviewClose"
+        else
+          vim.cmd "DiffviewOpen"
+        end
+      end, { desc = "Toggle Git diff view" })
+    end,
+  },
+  {
     "NeogitOrg/neogit",
     lazy = true,
     dependencies = {
